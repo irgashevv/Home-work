48b916ca16038e0f271187f72178b35c0971a6b5
Ibrohim <irgashev1261@gmail.com>
2022-03-24 12:08:37 +0500

    Realize Request class

---
+++ app/core/Request.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\core;
+
+class Request
+{
+    public static function url()
+    {
+        return trim(
+            parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
+        );
+    }
+
+    public static function method()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+}
